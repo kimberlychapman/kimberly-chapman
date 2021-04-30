4e9f75707457e0e280432936174238e9c8ac53a2
kimberlychapman <63875440+kimberlychapman@users.noreply.github.com>
2021-04-29 23:30:35 -0500

    changed the index pages around

--- script.js
+++ script.js
@@ -87,3 +87,30 @@ function closeRes(){
     document.getElementById('designer').style.display="flex";
     document.getElementById('about').style.display="flex";
 }
+
+// Dropdown Menu
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+dropdownArray.forEach(function(el){
+	var button = el.querySelector('a[data-toggle="dropdown"]'),
+			menu = el.querySelector('.dropdown-menu');
+
+	button.onclick = function(event) {
+		if(!menu.hasClass('show')) {
+			menu.classList.add('show');
+			menu.classList.remove('hide');
+			event.preventDefault();
+            document.getElementById('caret').src="images/caret-up.png";
+		}
+		else {
+			menu.classList.remove('show');
+			menu.classList.add('hide');
+			event.preventDefault();
+            document.getElementById('caret').src="images/caret-down.png";
+		}
+	};
+})
+
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
